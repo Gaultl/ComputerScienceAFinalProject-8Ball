269144177458e6d2b394e0c15c52c88828effc89
WLHS-CTE <gaultl@wlhs.wlwv.k12.or.us>
2023-05-17 09:56:14 -0700

    tried to make rotation go

--- main.py
+++ main.py
@@ -2,13 +2,32 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
-screen = pygame.display.set_mode((976, 530))
+screen = pygame.display.set_mode((903, 490))
 background = pygame.image.load("Pool-Table.png")
-background = pygame.transform.scale(background, (976, 530))
+# background = pygame.transform.scale(background, (903, 490))
 running = True
+
+#pool cue?
+# class Cue():
+#     def __init__(self, pos):
+#         cue_image = pygame.image.load("Pool-Cue.png").convert_alpha()
+#         self.image = cue_image
+#         self.angle = 0
+#         self.new_image = pygame.transform.rotate(self.image, self.angle)
+#         self.rect = self.image.get_rect()
+#         self.rect.center = pos
+
 
 while running:
     screen.blit(background, (0, 0))
+    cue = pygame.image.load("Pool-Cue.png").convert_alpha()
+    screen.blit(cue, (200, 200))
+    angle = 0
+    keys = pygame.key.get_pressed()
+
+    angle += 50
+    pygame.transform.rotate(cue, angle)
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == QUIT:
