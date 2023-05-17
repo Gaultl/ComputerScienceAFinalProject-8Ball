bf057f2847f8219c7b603e12224111fe17907225
delcarlswlhs <113708789+delcarlswlhs@users.noreply.github.com>
2023-05-17 09:17:50 -0700

    Update and rename main.py to pooltable.py

---
+++ pooltable.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((903, 490))
+pygame.display.set_caption('Loading image')
+#
+# pool_table = pygame.image.load('pooltable.png')
+# while True:
+#     screen.fill("black")
+#     screen.blit(pool_table, (0,0))
+#
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             quit()
+#         pygame.display.update()
