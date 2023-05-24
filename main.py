b2e388d522e5af377bb620d6b0ac76a79e92d24f
WLHS-CTE <gaultl@wlhs.wlwv.k12.or.us>
2023-05-24 09:41:14 -0700

    made rotation to a set angle work if key is pressed

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import pygame
 from pygame.locals import *
+
 pygame.init()
 
 screen = pygame.display.set_mode((903, 490))
 background = pygame.image.load("Pool-Table.png")
-# background = pygame.transform.scale(background, (903, 490))
+
 running = True
 
-#pool cue?
-# class Cue():
-#     def __init__(self, pos):
-#         cue_image = pygame.image.load("Pool-Cue.png").convert_alpha()
-#         self.image = cue_image
-#         self.angle = 0
-#         self.new_image = pygame.transform.rotate(self.image, self.angle)
-#         self.rect = self.image.get_rect()
-#         self.rect.center = pos
-
-
+image = pygame.image.load("Pool-Cue.png")
+mov_image = pygame.image.load("Pool-Cue.png")
+screen.blit(background, (0, 0))
+screen.blit(image, (100, 100))
 while running:
-    screen.blit(background, (0, 0))
-    cue = pygame.image.load("Pool-Cue.png").convert_alpha()
-    screen.blit(cue, (200, 200))
     angle = 0
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        angle += 90
+        image = pygame.transform.rotate(mov_image, angle)
+        rect = image.get_rect()
+        if angle > 90:
 
-    angle += 50
-    pygame.transform.rotate(cue, angle)
-    pygame.display.update()
+        screen.blit(background, (0, 0))
+        screen.blit(image, (100, 100))
+
+    if keys[pygame.K_LEFT]:
+        angle -= 90
+        image = pygame.transform.rotate(mov_image, angle)
+        rect = image.get_rect()
+        x = rect.width
+        screen.blit(background, (0, 0))
+        screen.blit(image, (100 - x, 100))
 
     for event in pygame.event.get():
         if event.type == QUIT:
