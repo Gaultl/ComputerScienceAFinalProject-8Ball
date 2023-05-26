67b2a68f2e88b21efdcf837425672caaddfc6b1e
WLHS-CTE <gaultl@wlhs.wlwv.k12.or.us>
2023-05-26 09:14:59 -0700

    Rotation works smoothly, placement of cue between 180 and 90 degrees and using the right key is not working

--- main.py
+++ main.py
@@ -12,35 +12,51 @@ image = pygame.image.load("Pool-Cue.png")
 mov_image = pygame.image.load("Pool-Cue.png")
 screen.blit(background, (0, 0))
 screen.blit(image, (100, 100))
+angle = 0
+center_x = 100
+center_y = 100
+x = 0
+y = 0
 while running:
-    angle = 0
-    center_x = 100
-    center_y = 100
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
-        angle += 100
+        angle += 1
         image = pygame.transform.rotate(mov_image, angle)
         rect = image.get_rect()
         y = rect.height
-        if abs(angle) > 90:
+        x = rect.width
+        if angle > 90:
             screen.blit(background, (0, 0))
-            screen.blit(image, (center_x, center_y - y))
+            screen.blit(image, (center_x + x, center_y - y))
+        if angle < -90 and angle < 0:
+            screen.blit(background, (0, 0))
+            screen.blit(image, (center_x - x, center_y - y))
         else:
             screen.blit(background, (0, 0))
             screen.blit(image, (center_x, center_y))
 
     if keys[pygame.K_LEFT]:
-        angle -= 100
+        angle -= 1
         image = pygame.transform.rotate(mov_image, angle)
         rect = image.get_rect()
         x = rect.width
         y = rect.height
-        if abs(angle) > 90:
+        if angle > 90:
+            screen.blit(background, (0, 0))
+            screen.blit(image, (center_x + x, center_y - y))
+        if angle < -90 and angle < 0:
             screen.blit(background, (0, 0))
             screen.blit(image, (center_x - x, center_y - y))
         else:
             screen.blit(background, (0, 0))
             screen.blit(image, (center_x - x, center_y))
+
+    if angle < -180:
+        angle = 360 + angle
+
+    if angle > 180:
+        angle = -360 + angle
+
 
     for event in pygame.event.get():
         if event.type == QUIT:
