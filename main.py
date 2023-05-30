16c644d5e79c3bb985ef86868fdcdad509aaf1a5
WLHS-CTE <gaultl@wlhs.wlwv.k12.or.us>
2023-05-30 09:54:17 -0700

    Rotation works smoothly, working on moving the center point of the cue

--- main.py
+++ main.py
@@ -10,15 +10,31 @@ running = True
 
 image = pygame.image.load("Pool-Cue.png")
 mov_image = pygame.image.load("Pool-Cue.png")
-screen.blit(background, (0, 0))
-screen.blit(image, (100, 100))
 angle = 0
 center_x = 100
 center_y = 100
+screen.blit(background, (0, 0))
+screen.blit(image, (center_x, center_y))
 x = 0
 y = 0
 while running:
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        center_y -= 1
+
+    if keys[pygame.K_s]:
+        center_y += 1
+
+    if keys[pygame.K_a]:
+        center_x -= 1
+
+    if keys[pygame.K_d]:
+        center_x += 1
+
+    screen.blit(background, (0, 0))
+    screen.blit(image, (center_x, center_y))
+
     if keys[pygame.K_RIGHT]:
         angle += 1
         image = pygame.transform.rotate(mov_image, angle)
@@ -27,11 +43,17 @@ while running:
         x = rect.width
         if angle > 90:
             screen.blit(background, (0, 0))
-            screen.blit(image, (center_x + x, center_y - y))
-        if angle < -90 and angle < 0:
+            screen.blit(image, (center_x, center_y - y))
+
+        if angle < -90:
             screen.blit(background, (0, 0))
             screen.blit(image, (center_x - x, center_y - y))
-        else:
+
+        if 0 > angle > -90:
+            screen.blit(background, (0, 0))
+            screen.blit(image, (center_x - x, center_y))
+
+        if 90 > angle > 0:
             screen.blit(background, (0, 0))
             screen.blit(image, (center_x, center_y))
 
@@ -43,20 +65,25 @@ while running:
         y = rect.height
         if angle > 90:
             screen.blit(background, (0, 0))
-            screen.blit(image, (center_x + x, center_y - y))
-        if angle < -90 and angle < 0:
+            screen.blit(image, (center_x, center_y - y))
+
+        if angle < -90:
             screen.blit(background, (0, 0))
             screen.blit(image, (center_x - x, center_y - y))
-        else:
+
+        if 0 > angle > -90:
             screen.blit(background, (0, 0))
             screen.blit(image, (center_x - x, center_y))
+
+        if 90 > angle > 0:
+            screen.blit(background, (0, 0))
+            screen.blit(image, (center_x, center_y))
 
     if angle < -180:
         angle = 360 + angle
 
     if angle > 180:
         angle = -360 + angle
-
 
     for event in pygame.event.get():
         if event.type == QUIT:
